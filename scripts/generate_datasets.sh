b9825f5befa62dcfa223ecf185474b56b9074815
Andrew Hundt <ATHundt@gmail.com>
2021-12-09 15:28:45 -0500

    TEMP DO NOT MERGE generate_datasets.sh generate test sets only

--- scripts/generate_datasets.sh
+++ scripts/generate_datasets.sh
@@ -10,12 +10,15 @@ echo "Generating dataset... Folder: $DATA_DIR"
 #############################
 ## Language-Conditioned Tasks
 
-LANG_TASKS='align-rope assembling-kits-seq-seen-colors assembling-kits-seq-unseen-colors packing-shapes packing-boxes-pairs-seen-colors packing-boxes-pairs-unseen-colors packing-seen-google-objects-seq packing-unseen-google-objects-seq packing-seen-google-objects-group packing-unseen-google-objects-group put-block-in-bowl-seen-colors put-block-in-bowl-unseen-colors stack-block-pyramid-seq-seen-colors stack-block-pyramid-seq-unseen-colors separating-piles-seen-colors separating-piles-unseen-colors towers-of-hanoi-seq-seen-colors towers-of-hanoi-seq-unseen-colors'
+# LANG_TASKS='align-rope assembling-kits-seq-seen-colors assembling-kits-seq-unseen-colors packing-shapes packing-boxes-pairs-seen-colors packing-boxes-pairs-unseen-colors packing-seen-google-objects-seq packing-unseen-google-objects-seq packing-seen-google-objects-group packing-unseen-google-objects-group put-block-in-bowl-seen-colors put-block-in-bowl-unseen-colors stack-block-pyramid-seq-seen-colors stack-block-pyramid-seq-unseen-colors separating-piles-seen-colors separating-piles-unseen-colors towers-of-hanoi-seq-seen-colors towers-of-hanoi-seq-unseen-colors'
+# LANG_TASKS=('align-rope' 'assembling-kits-seq-seen-colors' 'assembling-kits-seq-unseen-colors' 'packing-shapes' 'packing-boxes-pairs-seen-colors' 'packing-boxes-pairs-unseen-colors' 'packing-seen-google-objects-seq' 'packing-unseen-google-objects-seq' 'packing-seen-google-objects-group' 'packing-unseen-google-objects-group' 'put-block-in-bowl-seen-colors' 'put-block-in-bowl-unseen-colors' 'stack-block-pyramid-seq-seen-colors' 'stack-block-pyramid-seq-unseen-colors' 'separating-piles-seen-colors' 'separating-piles-unseen-colors' 'towers-of-hanoi-seq-seen-colors' 'towers-of-hanoi-seq-unseen-colors')
 
-for task in $LANG_TASKS
+
+# for task in $LANG_TASKS
+for task in align-rope assembling-kits-seq-seen-colors assembling-kits-seq-unseen-colors packing-shapes packing-boxes-pairs-seen-colors packing-boxes-pairs-unseen-colors packing-seen-google-objects-seq packing-unseen-google-objects-seq packing-seen-google-objects-group packing-unseen-google-objects-group put-block-in-bowl-seen-colors put-block-in-bowl-unseen-colors stack-block-pyramid-seq-seen-colors stack-block-pyramid-seq-unseen-colors separating-piles-seen-colors separating-piles-unseen-colors towers-of-hanoi-seq-seen-colors towers-of-hanoi-seq-unseen-colors
     do
-        python cliport/demos.py n=1000 task=$task mode=train data_dir=$DATA_DIR disp=$DISP &
-        python cliport/demos.py n=100  task=$task mode=val   data_dir=$DATA_DIR disp=$DISP &
+        # python cliport/demos.py n=1000 task=$task mode=train data_dir=$DATA_DIR disp=$DISP &
+        # python cliport/demos.py n=100  task=$task mode=val   data_dir=$DATA_DIR disp=$DISP &
         python cliport/demos.py n=100  task=$task mode=test  data_dir=$DATA_DIR disp=$DISP
     done
 echo "Finished Language Tasks."
@@ -24,14 +27,14 @@ echo "Finished Language Tasks."
 #########################
 ## Demo-Conditioned Tasks
 
-DEMO_TASKS='align-box-corner assembling-kits block-insertion manipulating-rope packing-boxes palletizing-boxes place-red-in-green stack-block-pyramid sweeping-piles towers-of-hanoi'
+# DEMO_TASKS='align-box-corner assembling-kits block-insertion manipulating-rope packing-boxes palletizing-boxes place-red-in-green stack-block-pyramid sweeping-piles towers-of-hanoi'
 
-for task in $DEMO_TASKS
-    do
-        python cliport/demos.py n=1000 task=$task mode=train data_dir=$DATA_DIR disp=$DISP &
-        python cliport/demos.py n=100  task=$task mode=val   data_dir=$DATA_DIR disp=$DISP &
-        python cliport/demos.py n=100  task=$task mode=test  data_dir=$DATA_DIR disp=$DISP
-    done
-echo "Finished Demo Tasks."
+# for task in $DEMO_TASKS
+#     do
+#         python cliport/demos.py n=1000 task=$task mode=train data_dir=$DATA_DIR disp=$DISP &
+#         python cliport/demos.py n=100  task=$task mode=val   data_dir=$DATA_DIR disp=$DISP &
+#         python cliport/demos.py n=100  task=$task mode=test  data_dir=$DATA_DIR disp=$DISP
+#     done
+# echo "Finished Demo Tasks."
 
 
