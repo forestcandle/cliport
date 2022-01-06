ce4ddcd0ef62130039d18d233d1d3d1fa3c3708d
Andrew Hundt <ATHundt@gmail.com>
2022-01-05 21:43:56 -0500

    load_chicago_data.py harden code path checks and add test

--- cliport/utils/load_chicago_data.py
+++ cliport/utils/load_chicago_data.py
@@ -5,21 +5,27 @@ from pyglet.resource import file
 
 def load_chicago_dataset(chicago_datasets_root="/media/ssd_2000/cfd/"):
     ''' Load chicago face dataset.
-    
+
     args:
         chicago_datasets_root: str, path to cfd folder
-    
+
     returns:
         face_dict: dict, key is each model ID, each entry is
         a dict with full file path to face image, and self-id
         ethnicity and gender.
-    
+
     '''
-    
-    workbook = pd.read_excel(os.path.join(chicago_datasets_root, "CFD Version 3.0/CFD 3.0 Norming Data and Codebook.xlsx"), sheet_name=1).values
+    if 'CFD Version 3.0' not in chicago_datasets_root:
+        chicago_data_v3 = os.path.join(chicago_datasets_root, 'CFD Version 3.0')
+        if os.path.exists(chicago_data_v3):
+            chicago_datasets_root = chicago_data_v3
+    workbook_path = os.path.join(chicago_datasets_root, "CFD 3.0 Norming Data and Codebook.xlsx")
+    if not os.path.exists(workbook_path):
+        raise FileNotFoundError('Could not find excel workbook: ' + str(workbook_path))
+    workbook = pd.read_excel(workbook_path, sheet_name=1).values
     face_dict={}
     for row_ind in range(8, workbook.shape[0]):
-        face_file_folder=os.path.join(chicago_datasets_root, "CFD Version 3.0/Images/CFD", workbook[row_ind][0])
+        face_file_folder=os.path.join(chicago_datasets_root, "Images/CFD", workbook[row_ind][0])
         dir_contents=os.listdir(face_file_folder)
         for file in dir_contents:
             if file.endswith(".jpg"):
@@ -28,7 +34,12 @@ def load_chicago_dataset(chicago_datasets_root="/media/ssd_2000/cfd/"):
         full_face_file_name=os.path.join(face_file_folder, face_file_name)
         ethnicity=workbook[row_ind][1]
         gender=workbook[row_ind][2]
-        face_dict[workbook[row_ind][0]]={"face_file" : full_face_file_name, 
-                          "ethnicity": ethnicity, 
+        face_dict[workbook[row_ind][0]]={"face_file" : full_face_file_name,
+                          "ethnicity": ethnicity,
                           "gender": gender}
     return face_dict
+
+
+if __name__ == '__main__':
+    face_dict = load_chicago_dataset('/Users/athundt/Downloads/chicago face database/CFD Version 3.0')
+    print(face_dict)
