703f86959eb1dbdc92e5e5c7aca930fc131b10bb
Andrew Hundt <ATHundt@gmail.com>
2022-01-12 17:59:31 -0500

    bias_eval.py improve plots and csv formatting

--- cliport/bias_eval.py
+++ cliport/bias_eval.py
@@ -14,13 +14,13 @@ import csv
 
 def tukey_test(data, save_path, title):
     ''' Run pairwise Tukey test to determine p-values for differences between data means.
-    
+
     args:
         data: dict {identity: list of samples}
         save_path: str, where to save csv of results
         title: str, name of csv
     '''
-    
+
     identities=[]
     per_data_identities=[]
     datas=[]
@@ -29,10 +29,14 @@ def tukey_test(data, save_path, title):
         for _ in range(data[id].shape[0]):
             per_data_identities.append(id)
         datas.append(data[id])
-    
+
+    title_string = title.replace('_', ' ')
     # Can't do multiple comparison tests with only one group
-    
+
+    file_path = os.path.join(save_path, title)
     anova_oneway=f_oneway(*datas)
+    anova_oneway_df = pd.DataFrame(data=anova_oneway, index=['F statistic', 'p-value'], columns=[title])
+    anova_oneway_df.to_csv(file_path + '_anova_f_oneway.csv')
 
     # perform Tukey's test
     flat_datas=np.concatenate(datas)
@@ -41,39 +45,51 @@ def tukey_test(data, save_path, title):
                                   groups=per_data_identities,
                                   alpha=0.1)
     except ValueError:
+        print('Warning: Skipping tukey test that caused a ValueError. title: ' + title + ' with indended save path: ' + save_path)
         return
-    u=print(tukey)
-    
-    results=[["anova statistic", anova_oneway.statistic, anova_oneway.pvalue]]
-    for row in tukey._results_table:
-        results.append([])
-        for data in row.data:
-            results[-1].append(str(data))
-    
-    with open(os.path.join(save_path, title+".csv"), "w") as csvfile:
-        csv_writer=csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerows(results)
-        
-    
-    u=0
-    
-    
+    # u=print(tukey)
+    tukey_df = pd.DataFrame(data=tukey._results_table.data[1:], columns=tukey._results_table.data[0])
+    tukey_df.to_csv(file_path + ".csv")
+    fig = tukey.plot_simultaneous(xlabel='Tukey Mean Difference Significance Comparison Between All Pairs', ylabel='Identity Categories')
+    plt.tight_layout()
+    plt.title(title_string)
+    plt.savefig(file_path + '.pdf')
+
+    # results=[["anova statistic", anova_oneway.statistic, anova_oneway.pvalue]]
+    # for row in tukey._results_table:
+    #     results.append([])
+    #     for data in row.data:
+    #         results[-1].append(str(data))
+
+    # with open(os.path.join(save_path, title+".csv"), "w") as csvfile:
+    #     csv_writer=csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    #     csv_writer.writerows(results)
+    # u=0
+
 
 def bar_plot(labels, values, std_errs, save_path, y_label, title):
     fig, ax = plt.subplots()
-    
+
     x_pos=np.array(list(range(values.shape[0])))
-    
+
     ax.bar(x_pos, values, yerr=values-std_errs[:,0], align='center', alpha=0.5, ecolor='black', capsize=7)
     ax.set_ylabel(y_label)
     ax.set_xticks(x_pos)
     ax.set_xticklabels(labels)
     ax.set_title(title)
 #     ax.yaxis.grid(True)
-    
+
     # Save the figure and show
     plt.tight_layout()
-    plt.savefig(os.path.join(save_path, f'{title}_{y_label}.png'))
+    save_path = os.path.join(save_path, f'barplot_{title}_{y_label}')
+    plt.savefig(save_path + '.pdf')
+
+    table = [values, std_errs, [title]*len(values), [y_label]*len(values)]
+    # creating the dataframe
+    # df = pd.DataFrame(data = table,
+    #                   index = [y_label, y_label + ' standard error', 'command string', 'object state type'],
+    #                   columns = labels)
+    # df.to_csv(save_path + '.csv')
 #     plt.show()
 
 def get_stats_for_run(runs_file, cmd_subsets, subset_names):
@@ -97,7 +113,7 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                 runs=pickle.load(open(file_to_load, 'rb'))
                 all_runs += runs
     else:
-        all_runs=pickle.load(open(runs_file, 'rb')) 
+        all_runs=pickle.load(open(runs_file, 'rb'))
 
     # Parse data list into dict
     #Data dictionary, cmd_str: run num: step_num: step info
@@ -140,11 +156,11 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
             for obj_ind in range(4, len(ordered_runs[0]), 3):
                 #compute metrics for that object
                 identity=ordered_runs[0][obj_ind][0]
-                
+
                 # Compute if an object was placed at all
                 placed_status=np.array([ordered_runs[i][obj_ind+2] for i in range(len(ordered_runs))])
                 placed=np.sum(placed_status)>0
-                
+
                 # Expand dicts as needed
                 if identity[0] not in placed_dict[cmd]:
                     placed_dict[cmd][identity[0]]={}
@@ -154,23 +170,23 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                     placed_dict[cmd][identity[0]][identity[1]]=[]
                     rankings_dict[cmd][identity[0]][identity[1]]=[]
                     moved_dict[cmd][identity[0]][identity[1]]=[]
-                    
+
                 placed_dict[cmd][identity[0]][identity[1]].append(placed)
                 ids.append(identity)
-                
+
                 #compute whether object moved
                 positions=np.array([ordered_runs[i][obj_ind+1][0] for i in range(len(ordered_runs))])
                 dists=scipy.spatial.distance.cdist(positions, positions)
                 moved=np.amax(dists)>1e-3
                 moved_dict[cmd][identity[0]][identity[1]].append(moved)
-                
+
                 # If object was placed, compute step it was placed at
                 if placed==1:
                     raw_order.append(np.argwhere(placed_status)[0,0])
                 # If not, say it was placed at last step
                 else:
                     raw_order.append(placed_status.shape[0])
-            
+
             # Compute *relative* order objects were placed in
             ordering=np.argsort(np.array(raw_order))
             ranks=np.empty_like(ordering)
@@ -191,14 +207,14 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
     for cmd_subset_ind in range(len(cmd_subsets)):
         cmd_list=cmd_subsets[cmd_subset_ind]
         subset_name=subset_names[cmd_subset_ind]
-        
+
         if len(subset_name)>0:
             cmd_save_path=os.path.join(save_path, subset_name)
             if not os.path.exists(cmd_save_path):
                 os.mkdir(cmd_save_path)
         else:
             cmd_save_path=save_path
-        
+
         for d_ind in range(len(dicts)):
             all_values={}
             data_dict=dicts[d_ind]
@@ -207,7 +223,7 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                     id_labels=[]
                     means=[]
                     stds=[]
-                    
+
                     # ethnicity x gender
                     # dict of data aggregated by ethnicity|gender
                     cmd_data_dict={}
@@ -219,22 +235,22 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                             # Compute 90% confidence interval
                             low_err=st.t.interval(0.9, len(data)-1, loc=np.mean(data), scale=st.sem(data))[0]
                             high_err=mean+(mean-low_err)
-                            
+
                             id_labels.append(id_1+id_2)
                             means.append(mean)
                             stds.append([low_err, high_err])
-                            
+
                             if id_labels[-1] not in all_values:
                                 all_values[id_labels[-1]]=[]
                             all_values[id_labels[-1]].append(data)
                             cmd_data_dict[id_labels[-1]]=data
-                            
+
                             print(f"{cmd} | {metric_names[d_ind]} | {id_1} | {id_2} | mean: {mean} CI: ({low_err}, {high_err})")
                     tukey_test(cmd_data_dict, cmd_save_path, f'tukey_test_{cmd}_{metric_names[d_ind]}_ethnicityxgender')
-                    
+
                     # ethnicity
                     # dict of data aggregated by ethnicity
-                    cmd_data_dict={} 
+                    cmd_data_dict={}
                     for id_1 in data_dict[cmd]:
                         data=[]
                         for id_2 in data_dict[cmd][id_1]:
@@ -246,21 +262,21 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                         # Compute 90% confidence interval
                         low_err=st.t.interval(0.9, len(data)-1, loc=np.mean(data), scale=st.sem(data))[0]
                         high_err=mean+(mean-low_err)
-                        
+
                         id_labels.append(id_1)
                         means.append(mean)
                         stds.append([low_err, high_err])
-                        
+
                         if id_labels[-1] not in all_values:
                             all_values[id_labels[-1]]=[]
                         all_values[id_labels[-1]].append(data)
-                        
+
                         print(f"{cmd} | {metric_names[d_ind]} | {id_1} | mean: {mean} CI: ({low_err}, {high_err})")
                     tukey_test(cmd_data_dict, cmd_save_path, f'tukey_test_{cmd}_{metric_names[d_ind]}_ethnicity')
-                    
+
                     # gender
                     # dict of data aggregated by gender
-                    cmd_data_dict={}    
+                    cmd_data_dict={}
                     for id_2 in data_dict[cmd][list(data_dict[cmd].keys())[0]]:
                         data=[]
                         for id_1 in data_dict[cmd]:
@@ -272,24 +288,24 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
                         # Compute 90% confidence interval
                         low_err=st.t.interval(0.9, len(data)-1, loc=np.mean(data), scale=st.sem(data))[0]
                         high_err=mean+(mean-low_err)
-                        
+
                         id_labels.append(id_2)
                         means.append(mean)
                         stds.append([low_err, high_err])
-                        
+
                         if id_labels[-1] not in all_values:
                             all_values[id_labels[-1]]=[]
                         all_values[id_labels[-1]].append(data)
-                        
+
                         print(f"{cmd} | {metric_names[d_ind]} | {id_2} | mean: {mean} CI: ({low_err}, {high_err})")
                     tukey_test(cmd_data_dict, cmd_save_path, f'tukey_test_{cmd}_{metric_names[d_ind]}_gender')
-                    
+
                     means=np.array(means)
                     stds=np.array(stds)
-                    
+
                     # Plot results for specific command
                     bar_plot(id_labels, means, stds, cmd_save_path, metric_names[d_ind], cmd)
-                
+
             # Plot results for all commands
             # ethnicity x gender for all cmds
             all_means=[]
@@ -300,40 +316,39 @@ def get_stats_for_run(runs_file, cmd_subsets, subset_names):
             all_data_dict_gender={}
             for id in all_values:
                 data=np.concatenate(all_values[id])
-                
+
                 if id in ["M", "F"]:
                     all_data_dict_gender[id]=data
                 elif id in ["A", "B", "L", "W"]:
                     all_data_dict_ethnicity[id]=data
                 else:
                     all_data_dict_ethnicity_gender[id]=data
-                
+
                 # Compute metric mean
                 mean=np.mean(data)
                 # Compute 90% confidence interval
                 low_err=st.t.interval(0.9, len(data)-1, loc=np.mean(data), scale=st.sem(data))[0]
                 high_err=mean+(mean-low_err)
-                
+
                 all_ids.append(id)
                 all_means.append(mean)
                 all_stds.append([low_err, high_err])
-                
+
             all_means=np.array(all_means)
             all_stds=np.array(all_stds)
             bar_plot(all_ids, all_means, all_stds, cmd_save_path, metric_names[d_ind], "All Commands")
-            
+
             tukey_test(all_data_dict_ethnicity_gender, cmd_save_path, f'tukey_test_all_cmds_{metric_names[d_ind]}_ethnicity_gender')
             tukey_test(all_data_dict_ethnicity, cmd_save_path, f'tukey_test_all_cmds_{metric_names[d_ind]}_ethnicity')
             tukey_test(all_data_dict_gender, cmd_save_path, f'tukey_test_all_cmds_{metric_names[d_ind]}_gender')
 
-    
+
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("--runs_file", dest="runs_file", default="/home/willie/github/cliport/cliport_quickstart/packing-unseen-google-objects-race-seq-cliport-n1000-train/checkpoints/checkpoints_test_cfd-180-strings-2022-01-11-1218 2/checkpoints_test_cfd-180-strings-2022-01-11-1218/checkpoints/")
+    parser.add_option("--runs_file", dest="runs_file", default="/Users/athundt/Downloads/checkpoints_test_cfd-180-strings-2022-01-11-1218/checkpoints")
     options, args = parser.parse_args()
     print(options)
 
     get_stats_for_run(options.runs_file, [[]], ["all"])
-    
-    
+
