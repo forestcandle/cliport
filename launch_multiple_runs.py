75815691acdd95aa6bedbd7d169babd0aafd0446
willie <wagnew3@cs.washington.edu>
2022-03-23 23:55:51 -0700

    run exps in paper

--- launch_multiple_runs.py
+++ launch_multiple_runs.py
@@ -4,8 +4,8 @@ import itertools
 
 if __name__ == '__main__':
     args=[]
-    num_sentances_per=5
-    for sentance_ind in range(0, 10, num_sentances_per):
+    num_sentances_per=1
+    for sentance_ind in [28, 137, 0, 50, 51, 52, 77, 78, 79, 83, 84, 85, 56, 57, 58]:
         args.append(f"command_string_min={sentance_ind} command_string_max={sentance_ind+num_sentances_per} num_processes=20")
     
     for ind in range(len(args)):
